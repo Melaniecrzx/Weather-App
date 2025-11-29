3ca815433e5d54e1d3720f8bf4be1cfa78d79d36
Melaniecrzx <melanie.crzx@outlook.fr>
2025-11-29 18:24:55 +0100

    "push commit"

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Weather-App/', // ⚠️ Ajoutez cette ligne pour GitHub Pages
 })
